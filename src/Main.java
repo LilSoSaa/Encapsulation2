12cec1bc3bf74abe7577881fdbb09f38a012bbe7
giova <giovannitopor@gmail.com>
2023-01-29 23:54:31 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String ...args) {
+
+    Person p1= new Person();
+    p1.name= "Giovanni";
+    p1.surname= "Bianchi";
+    p1.age= 18;
+    p1.height= 1.78;
+
+        System.out.println(p1.toString());
+    }
+
+
+}
---
+++ src/Person.java
@@ -0,0 +1,12 @@
+import lombok.Data;
+import lombok.ToString;
+@ToString
+
+public class Person {
+
+     public   String name;
+     public String surname;
+     public double height;
+     public int age;
+
+}
